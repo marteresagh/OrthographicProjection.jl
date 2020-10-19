33c21ff60199b528d1bc1ea513f449e89f77183a
marteresagh <marteresa28@gmail.com>
2020-10-19 11:23:55 +0200

    update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -6,9 +6,10 @@ txtpotreedirs = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\directory.txt"
 potreedirs = FileManager.get_directories(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
-bbin = "C:/Users/marte/Documents/FilePotree/cava.json"
-
+#bbin = "C:/Users/marte/Documents/FilePotree/cava.json"
+ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.3
 PO = "XY+"
 outputimage = "examples/Projection_CAVA_$PO.jpg"
-@time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, nothing, nothing, Lar. )
+
+@time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, nothing, nothing, ucs, false )
