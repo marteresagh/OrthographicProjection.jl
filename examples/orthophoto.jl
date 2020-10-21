d13284abadec636efaae1af78d4fdbb53da47b7e
marteresagh <marteresa28@gmail.com>
2020-10-21 10:06:13 +0200

    update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -2,18 +2,17 @@ using OrthographicProjection
 using FileManager
 using Common
 
-txtpotreedirs = "C:/Users/marte/Documents/GEOWEB/FilePotree/orthoCAVA/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/GEOWEB/FilePotree/orthoCONTEA/directory.txt"
 potreedirs = FileManager.get_directories(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 #bbin = metadata.tightBoundingBox
-bbin = Common.return_AABB(([458117.67, 4.49376852e6, 208.67], [458452.44, 4.49417179e6, 212.5]))
 #bbin = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCAVA\\volume.json"
 ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.3
 PO = "XY+"
-quota = nothing #458277.430, 4493982.030, 210.840
-thickness = nothing
-outputimage = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCAVA\\Projection_CAVA_$PO.jpg"
+quota = 2.50 #458277.430, 4493982.030, 210.840
+thickness = 0.05
+outputimage = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\Sezione_$quota.jpg"
 pc = true
 
 @time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, quota, thickness, ucs, pc )
