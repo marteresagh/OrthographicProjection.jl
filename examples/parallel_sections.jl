fcdc86c7e15d4d4429d5e40bda5a278736f4bc3b
marteresagh <marteresa28@gmail.com>
2020-11-05 09:18:04 +0100

    add example parallel section

--- examples/parallel_sections.jl
+++ examples/parallel_sections.jl
@@ -6,7 +6,6 @@ using Visualization
 txtpotreedirs = "C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = FileManager.get_directories(txtpotreedirs)
 all_files = FileManager.get_files_in_potree_folder(potreedirs[1],0)
-PC = FileManager.las2pointcloud(all_files...)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
 project_name = "Sezioni_Parallele"
@@ -14,7 +13,7 @@ output_folder = "C:/Users/marte/Documents/GEOWEB/TEST"
 plane = Plane(1,0,0, 458300)
 step = 30.0
 thickness = 1.
-@time planes = OrthographicProjection.get_parallel_sections(
+@time OrthographicProjection.get_parallel_sections(
 	txtpotreedirs::String,
 	project_name::String,
 	output_folder::String,
@@ -24,7 +23,11 @@ thickness = 1.
 	thickness::Float64)
 
 
+PC = FileManager.las2pointcloud(all_files...)
 GL.VIEW([
 	GL.GLPoints(convert(Lar.Points,PC.coordinates')),
 	[GL.GLGrid(planes[i][1],planes[i][2]) for i in 1:length(planes)]...
 ])
+
+
+# julia parallel_sections.jl "C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt" --bbin "458117.68 4.49376853e6 196.68 458452.43 4.49417178e6 237.49" -o "C:/Users/marte/Documents/GEOWEB/TEST" -p "Sezioni_Parallele" --step 30 --plane "1 0 0 458300" --thickness 1
