dc626cfde632b86dc35533d51841c3057da5420e
marteresagh <marteresa28@gmail.com>
2020-12-01 12:02:57 +0100

    batch update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -2,17 +2,17 @@ using OrthographicProjection
 using FileManager
 using Common
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CAVA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
-potreedirs = FileManager.get_directories(txtpotreedirs)
-metadata = CloudMetadata(txtpotreedirs)
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/LACONTEA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
+metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
 #bbin = "C://Users//marte//Documents//GEOWEB//FilePotree//orthoCAVA//volume.json"
 ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.3
 PO = "XY+"
-quota = nothing #458277.430, 4493982.030, 210.840
-thickness = nothing
-outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ortho_prova.jpg"
+quota = 4.918 #458277.430, 4493982.030, 210.840
+thickness = 0.52
+outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/pavimento.jpg"
 pc = true
 background = [0.0,0.0,0.0]
 @time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, quota, thickness, ucs, background, pc)
