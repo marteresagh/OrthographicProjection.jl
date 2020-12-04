9a062e7b089d7bc1cb6a92555d454386e6274567
marteresagh <marteresa28@gmail.com>
2020-12-04 16:41:07 +0100

    travis config

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,14 @@
-push!(LOAD_PATH,"../src/")
-
-using Documenter, OrthographicProjection
+using Documenter
+using OrthographicProjection
 
 makedocs(
 	format = :html,
 	sitename = "OrthographicProjection.jl",
 	assets = ["assets/OrthographicProjection.css", "assets/logo.jpg"],
-	pages = [
-		"1 - Home" => "index.md",
-		"2 - Getting Started" => "gettingStarted.md",
-		"Theory..." => [
-			"3.0 - Theory Index" => "theory-index.md",
-		],
-		"... and Practice" => [
-			"4.0 - Module Introduction" => "this-module.md",
-		],
-		"A - About the Authors" => "authors.md",
-		"B - Bibliography" => "bibliography.md"
-	]
+	modules = [OrthographicProjection]
+)
+
+
+deploydocs(
+	repo = "github.com/marteresagh/OrthographicProjection.jl.git"
 )
