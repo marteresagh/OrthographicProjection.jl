8f481bdf39ab226db94da861f5048786c1e1bf7a
marteresagh <marteresa28@gmail.com>
2020-12-09 11:05:32 +0100

    batch update

--- examples/debug.jl
+++ examples/debug.jl
@@ -3,11 +3,11 @@ using Visualization
 using Common
 using OrthographicProjection
 
-source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CAVA"
-files = FileManager.get_files_in_potree_folder(source,1)
+source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/MONTE_VETTORE"
+files = FileManager.get_files_in_potree_folder(source,0)
 PC = FileManager.las2pointcloud(files...)
 
-fname = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/CAVA_AABB.las"
+fname = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/MONTE_VETTORE_AABB.las"
 aabb = FileManager.las2aabb(fname)
 
 V,EV,FV = getmodel(aabb)
--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -3,7 +3,7 @@ using FileManager
 using Common
 # using Visualization
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CAVA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/MONTE_VETTORE" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
@@ -13,7 +13,7 @@ GSD = 0.3
 PO = "XZ+"
 quota = (bbin.y_max+bbin.y_min)/2 #458277.430, 4493982.030, 210.840
 thickness = 1.
-outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/CAVA_AABB.jpg"
+outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/MONTE_VETTORE_AABB.jpg"
 pc = true
 background = [0.0,0.0,0.0]
 @time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, quota, thickness, ucs, background, pc)
