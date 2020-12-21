8613fec2489bf6fe04a0ca92411993bc65a7ea97
marteresagh <marteresa28@gmail.com>
2020-12-21 11:58:55 +0100

    batch update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -3,16 +3,16 @@ using FileManager
 using Common
 # using Visualization
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CAVA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CASALETTO" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
 ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.03
-PO = "YZ-"
-quota = nothing #458277.430, 4493982.030, 210.840
-thickness = nothing
-outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/prova.jpg"
+PO = "XY+"
+quota = 105. #458277.430, 4493982.030, 210.840
+thickness = 0.05
+outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/casaletto_planimetria.jpg"
 pc = true
 background = [1.0,1.0,1.0]
 @time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, quota, thickness, ucs, background, pc)
