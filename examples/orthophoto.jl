eb48aec099269a505424f730a870169435f1a2ff
marteresagh <marteresa28@gmail.com>
2020-12-28 16:19:07 +0100

    batch update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -1,18 +1,32 @@
 using OrthographicProjection
 using FileManager
 using Common
-# using Visualization
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CASALETTO" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/LACONTEA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
 ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.03
 PO = "XY+"
-quota = 105. #458277.430, 4493982.030, 210.840
+quota = 2.50 #458277.430, 4493982.030, 210.840
 thickness = 0.05
-outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/prova.jpg"
-pc = true
-background = [1.0,1.0,1.0]
+outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/corner.jpg"
+pc = false
+background = [0.0,0.0,0.0]
 @time OrthographicProjection.orthophoto(txtpotreedirs, outputimage, bbin, GSD, PO, quota, thickness, ucs, background, pc)
+
+
+## VIEW
+# using Visualization
+# source = ""
+# files = FileManager.get_files_in_potree_folder(source,0)
+# PC = FileManager.las2pointcloud(files...)
+# V,EV,FV = getmodel(bbin)
+#
+# GL.VIEW(
+#     [
+#     GL.GLGrid(V,EV),
+#     Visualization.points_color_from_rgb(PC.coordinates,PC.rgbs)
+#     ]
+# )
