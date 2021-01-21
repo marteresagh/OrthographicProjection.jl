97065c9b6ba3e4f548702dc0e85ad848a8014dc3
marteresagh <marteresa28@gmail.com>
2021-01-21 11:31:14 +0100

    batch update

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -2,7 +2,7 @@ using OrthographicProjection
 using FileManager
 using Common
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/COLOMBELLA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CHIESA_COLOMBELLA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 # txtpotreedirs = "C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
@@ -10,10 +10,10 @@ bbin = metadata.tightBoundingBox
 bbin = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/volume_COLOMBELLA.json"
 ucs = Matrix{Float64}(Lar.I,3,3)
 GSD = 0.3
-PO = "YZ+"
-quota = nothing #458277.430, 4493982.030, 210.840
-thickness = nothing
-outputimage = "C:/Users/marte/Documents/GEOWEB/TEST/ORTHO/COLOMBELLA.jpg"
+PO = "XY+"
+quota = 273. #458277.430, 4493982.030, 210.840
+thickness = 0.05
+outputimage = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/sezione_CHIESA.jpg"
 pc = true
 background = [0.0,0.0,0.0]
 epsg = nothing #2049
