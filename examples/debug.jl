a9f7384172542ce7dac1b7b4918ccfa9654748ba
marteresagh <marteresa28@gmail.com>
2021-06-17 12:33:25 +0200

    apply ucs to laspoint

--- examples/debug.jl
+++ examples/debug.jl
@@ -3,17 +3,29 @@ using Visualization
 using Common
 using OrthographicProjection
 
+#
+# source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CASTELFERRETTI"
+# json = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/volume_CASTELFERRETTI.json"
+#
+# model = getmodel(json)
+# # PC = FileManager.source2pc(source,0)
+# aabb = AABB(108.553,-45.995,76.484,-78.06400000000001,150.082,-4.466)
+# model = getmodel(aabb)
+# GL.VIEW([
+# 	Visualization.points(PC.coordinates,PC.rgbs),
+# 	Visualization.points(center),
+# 	GL.GLGrid(model[1],model[2]),
+# 	Visualization.axis_helper()...
+# ])
+#
 
-source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CASTELFERRETTI"
-json = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/volume_CASTELFERRETTI.json"
 
-model = getmodel(json)
-# PC = FileManager.source2pc(source,0)
-aabb = AABB(108.553,-45.995,76.484,-78.06400000000001,150.082,-4.466)
+source = "./examples/CASALETTO_UCS_TEST005.las"
+PC = FileManager.source2pc(source,0)
+aabb = FileManager.las2aabb(source)
 model = getmodel(aabb)
-GL.VIEW([
+Visualization.VIEW([
 	Visualization.points(PC.coordinates,PC.rgbs),
-	Visualization.points(center),
-	GL.GLGrid(model[1],model[2]),
-	Visualization.axis_helper()...
+	Visualization.GLGrid(model[1],model[2]),
+	# Visualization.axis_helper()...
 ])
