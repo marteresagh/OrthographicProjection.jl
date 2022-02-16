b96e5d1b03baefd3252a67b10096b6d3f0c3012b
marteresagh <marteresa28@gmail.com>
2022-02-16 15:57:21 +0100

    update version of package

--- examples/orthophoto.jl
+++ examples/orthophoto.jl
@@ -3,12 +3,12 @@ using FileManager
 using Common
 using Visualization
 
-txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CAVA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
+txtpotreedirs = "C:/Users/marte/Documents/potreeDirectory/pointclouds/COLOMBELLA" #"C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 # txtpotreedirs = "C:/Users/marte/Documents/GEOWEB/wrapper_file/directory.txt"
 potreedirs = OrthographicProjection.get_potree_dirs(txtpotreedirs)
 metadata = CloudMetadata(potreedirs[1])
 bbin = metadata.tightBoundingBox
-#bbin = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/volume_COLOMBELLA.json"
+bbin = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/volume_COLOMBELLA.json"
 ucs = Matrix{Float64}(Common.I,4,4)
 GSD = 0.3
 PO = "XY+"
